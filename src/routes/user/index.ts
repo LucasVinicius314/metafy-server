d1e77a2fb2d0e996c1299fc808f76ac5ae22c672
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-04-04 21:06:36 -0300

    update profile

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,6 +1,8 @@
 import { HttpException } from '../../exceptions/httpexception'
 import { Models } from '../../services/sequelize'
 import { Router } from 'express'
+import { matches } from '../../utils/validation'
+import { sha256 } from '../../utils/crypto'
 import { sign } from '../../middleware/jwt'
 
 export const userRouter = Router()
@@ -28,7 +30,7 @@ userRouter.post('/validate', async (req, res, next) => {
   try {
     const user = await Models.User.findOne({
       attributes: {
-        exclude: ['password', 'email'],
+        exclude: ['password'],
       },
       where: {
         id: req.user.id,
@@ -36,6 +38,44 @@ userRouter.post('/validate', async (req, res, next) => {
     })
 
     res.setHeader('authorization', sign(user.get()))
+
+    res.json(user)
+  } catch (error) {
+    next(new HttpException(400, 'Invalid data'))
+  }
+})
+
+userRouter.post('/update', async (req, res, next) => {
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
+  try {
+    const user = await Models.User.findOne({
+      attributes: {
+        exclude: ['password', 'email'],
+      },
+      where: {
+        id: req.user.id,
+      },
+    })
+
+    const updateObject = {
+      username: username,
+      email: email,
+    }
+
+    matches(username, 'string', 'Invalid username')
+    matches(email, 'string', 'Invalid email')
+
+    if (password !== undefined) {
+      matches(password, 'string', 'Invalid password')
+      Object.assign(updateObject, { password: sha256(password) })
+    }
+
+    const updatedUser = await user.update(updateObject)
+
+    res.setHeader('authorization', sign(updatedUser.get()))
 
     res.json(user)
   } catch (error) {
